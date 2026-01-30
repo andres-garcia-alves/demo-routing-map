43f2ec2d0c08d53d1fa0cb28b25fa3bf1aaaea03
Andres Garcia Alves <andres_garcia_ao@hotmail.com>
2026-01-30 02:56:08 -0300

    Adding CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  base: '/demo-routing-map/',
+  base: '/app-travel-map/',
   plugins: [vue()],
   resolve: {
     alias: {
